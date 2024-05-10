e2998aae3f43846442733206ce76bbf06cfbd073
yash0922 <yashkumarpal987@gmail.com>
2024-05-10 21:09:43 +0530

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,73 @@
 import React, { useState } from "react";
-import { Box, Text, Button, Heading, Image } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, Text, Button, Heading, Image,AspectRatio  } from "@chakra-ui/react";
+import { css, keyframes } from '@emotion/react';
+import { ArrowForwardIcon,CheckIcon} from "@chakra-ui/icons";
 import { color, transform } from "framer-motion";
 import ServiceBox from "../components/ServiceBox";
+
+const imageAnimation1 = keyframes`0% {opacity: 0;} 25% {opacity: 1;} 50% {opacity: 0;} 75% {opacity: 0;} 100% {opacity: 0;}`;
+const imageAnimation2 = keyframes`0% { opacity: 0; } 25% { opacity: 0; } 50% { opacity: 0; } 75% { opacity: 1; } 100% { opacity: 0; }`;
+const imageAnimation3 = keyframes`0% { opacity: 0; } 25% { opacity: 0; } 50% { opacity: 1; } 75% { opacity: 0; } 100% { opacity: 0; }`;
+ const imageAnimation4 = keyframes`0% { opacity: 0; } 25% { opacity: 0; } 50% { opacity: 0; } 75% { opacity: 0; } 100% { opacity: 1; }`;
+
 function Home(props) {
   const [isHovered, setIsHovered] = useState(false);
   const [isHovered2, setIsHovered2] = useState(false);
- 
+  const cardsData = [
+    {
+      id: 1,
+      title: 'Exclusive Rights',
+      description: 'Our mission is to make an impact in empowering human society with AI. Hence, all Intellectual Property Rights belongs to you.',
+      imageUrl: 'https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fgreen.e71c8735.png&w=256&q=75',
+      bgColor: '#01c99b',
+      hoverBgGradient: 'linear-gradient(rgba(1, 201, 156, 0.15) 0%, rgba(1, 201, 156, 0) 100%, transparent)',
+      hoverOutline: 'rgb(1,201,155) solid 1px',
+      blurRadius: '170px',
+      offsetX: -50,
+      offsetY: -50,
+      imageAnimation: imageAnimation1,
+    },
+    {
+      id: 2,
+      title: 'Research Driven',
+      description: 'We regularly benchmark our solutions via comparing industry-vide evaluations so our partners only get the best that AI can offer.',
+      imageUrl: 'https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fblue.f22859fe.png&w=256&q=75',
+      bgColor: '#00b2fc',
+      hoverBgGradient: 'linear-gradient(rgba(3, 177, 249, 0.2) 0%, rgba(63, 189, 241, 0) 100%, transparent)',
+      hoverOutline: 'rgb(63,189,241) solid 1px',
+      blurRadius: '199px',
+      offsetX: 150,
+      offsetY: -55,
+      imageAnimation: imageAnimation2,
+  
+    },
+    {
+      id: 3,
+      title: 'Plug-and-Play',
+      description: 'We provide AI-driven solutions into businesses where they can bring tangible value. Each solution is customized as per your needs and deployed on any computing device of your choice.',
+      imageUrl: 'https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fred.a7efdb29.png&w=128&q=75',
+      bgColor: '#ff6160',
+      hoverBgGradient: 'linear-gradient(rgba(255, 107, 102, 0.2) 0%, rgba(0, 0, 0, 0) 100%, transparent)',
+      hoverOutline: 'rgb(255,96,95) solid 1px',
+      blurRadius: '170px',
+      offsetX: 20,
+      offsetY: 375,
+      imageAnimation: imageAnimation3,
+    },
+    {
+      id: 4,
+      title: 'Lifetime Support',
+      description: 'Should you face any issues, we are always at your service. We provide lifetime technical support & upgrade options.',
+      imageUrl: 'https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fyellow.d937d179.png&w=256&q=75',
+      bgColor: '#fbc976',
+      hoverBgGradient: 'linear-gradient(rgba(255, 159, 49, 0.15) 0%, rgba(255, 159, 49, 0) 100%, transparent)',
+      hoverOutline: 'rgb(255,159,49) solid 1px',
+      blurRadius: '170px',
+      offsetX: 60,
+      offsetY: -70,
+      imageAnimation: imageAnimation4,
+    },
+  ];
   const serviceData = [
     {
       id: 1,
@@ -59,7 +120,14 @@ function Home(props) {
       blurClassName: "yelloBlur",
     },
   ]
-
+  const gradientAnimation = keyframes`
+  0% {
+    background-position: 0;
+  }
+  100% {
+    background-position: -200%;
+  }
+`;
   return (
     <Box>
       <Box
@@ -302,6 +370,325 @@ function Home(props) {
         <Box display={"grid"} gridTemplateColumns={"repeat(2,1fr)"} gridGap={"20px"}>
           {serviceData.map((data, index) => (
             <ServiceBox key={index} data={data} />
+          ))}
+        </Box>
+      </Box>
+      <Box className="fourthContainer" display={"flex"} justifyContent={"center"} alignItems={"center"} w={"100%"} bg={"#fcfbf7"}>
+     <Box className="TrueAIBox" bg={"#272e5c"} w={"100%"} h={"550px"} zIndex={"10"} clipPath={"polygon(0 22.5%,100% 0,100% 100%,0 100%)"} display={"flex"} justifyContent={"space-between"} position={"relative"} alignItems={"center"} m={"6rem 0"}>
+            <Box className="trueBox_one" w={"33%"} zIndex={"20"} mt={"4rem"}>
+              <Image w={"333px"} h={"329px"} ml={"5rem"} src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FvectorAi.8ec84e23.png&w=384&q=75"
+              alt="trueAI" />
+            </Box>
+            <Box className="trueBox_two" display={"flex"} w={"33%"} mt={"4rem"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"}>
+              <Box className="MeetTrue" display={"flex"} fontFamily={"Poppins,sans-serif"} mb={"20px"} gap={"15px"} textTransform={"capitalize"} justifyContent={"center"} fontWeight={"600"} fontSize={"64px"} alignItems={"center"} color={"#fff"}>
+              Meet
+              <Box position={"relative"}>
+                <Box display={"flex"}   css={css`
+      color: transparent;
+      background-clip: text;
+      background-size: 200%;
+      animation: ${gradientAnimation} 2s linear infinite;
+      background-image: linear-gradient(
+        to right,
+        #9747ff 0%,  /* --tw-gradient-from */
+        #2f80ed 50%, /* --tw-gradient-via */
+        #ee216b 100%  /* --tw-gradient-to */
+      );
+      transition: all 0.5s ease;
+    `}>
+                Tru-AI
+                </Box>
+                <Image color={"transparent"} position={"absolute"} w={"200px"}  overflow={"clip"} h={"24px"} bottom={"-10px"} src="https://lenscorp.ai/_next/static/media/underline.27e9f7f7.svg"/>
+              </Box>
+              </Box>
+              <Box color={"#fff"} fontSize={"26px"} fontWeight={"500"}lineHeight={"normal"} mb={"15px"}>
+              Design. Create. Deploy.
+              </Box>
+              <Box display={"flex"}alignItems={"center"} justifyContent={"center"} gap={"10px"}>
+                <Box className="hypen" w={"2.5rem"} borderRadius={"10px"} borderBottom={"2px solid #bdbdbd"}></Box>
+                <Box fontSize={"20px"} display={"inline"} mb={"15px"} fontWeight={"600"}
+                 css={css`
+                 color: transparent;
+                 background-clip: text;
+                 background-size: 200%;
+                 animation: ${gradientAnimation} 2s linear infinite;
+                 background-image: linear-gradient(
+                   to right,
+                   #9747ff 0%,  /* --tw-gradient-from */
+                   #2f80ed 50%, /* --tw-gradient-via */
+                   #ee216b 100%  /* --tw-gradient-to */
+                 );
+                 transition: all 0.5s ease;
+               `}>the Future of AI | Power to EDIT</Box>
+              </Box>
+              <Box color={"#fff"} textAlign={"center"} fontSize={"14px"} textTransform={"capitalize"} mb={"15px"}>
+              Based On your website & traffic trends, Tru-AI optimises your website
+              </Box>
+              <Box
+              as="button"
+              lineHeight="1.2"
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              transition="all 0.3s cubic-bezier(.08,.52,.52,1)"
+              border="1px"
+              gap="15px"
+              mt="1rem"
+              p="10px 20px"
+              m="10px 0"
+              borderRadius="10px"
+              fontSize="18px"
+              fontWeight="semibold"
+              bg="transparent"
+              borderColor="#fff"
+              color="#fff"
+              _hover={{
+                bg: "#fff",
+                borderColor: "#fff",
+                color: "#000",
+                p: "10px 20px",
+              }}
+              _active={{
+                bg: "#fff",
+                transform: "scale(0.98)",
+                borderColor: "#000",
+              }}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              Learn More
+              {isHovered && <ArrowForwardIcon boxSize={6} />}
+            </Box>
+            </Box>
+            <Box className="trueBox_three" w={"33%"} display={"flex"} justifyContent={"flex-end"}>
+            
+  <video
+    style={{ objectFit: "contain", borderRadius:"1000px 0 0 1000px"  }}
+    autoPlay
+    loop
+    muted
+    playsInline
+    controls={false}
+  >
+    <source
+      src="https://res.cloudinary.com/detwrhe0k/video/upload/v1691730954/lenscorp-website/product-intro_1_dmajx1_qaemeu.mp4"
+      type="video/mp4"
+    />
+  </video>
+
+            </Box>
+     </Box>
+      </Box>
+      <Box fontFamily={"Poppins,sans-serif"} className="FifthContainer" display={"grid"} placeItems={"center"} pb={"4rem"} bg={"#fcfbf7"} overflow={"hidden"}>
+        <Box
+          className="third-one"
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={"5px"}
+          justifyContent={"center"}
+        >
+          <Heading
+            as={"h2"}
+            fontFamily={"Raleway,sans-serif"}
+            textTransform={"uppercase"}
+            letterSpacing={"1.9px"}
+            lineHeight={"normal"}
+            fontWeight={"600"}
+            fontStyle={"normal"}
+            textAlign={"center"}
+            fontSize={"20px"}
+            m={0}
+            mt="6rem"
+          >
+            WHY CHOOSE LENS
+          </Heading>
+          <Box w={"75px"} h={"4px"} borderRadius={"1000px"}
+            bg={
+              "linear-gradient(54deg, rgba(0,145,255,1) 0%, rgba(156,254,255,1) 100%)"
+            }></Box>
+        </Box>
+
+        <Text w={"80%"} fontFamily={"Poppins,sans-serif"} textAlign={"center"} fontSize={"58px"} fontStyle={"normal"} fontWeight={"400"} m={"58px 0px 10px"} lineHeight={"normal"}>AI-driven solutions backed by science</Text>
+        <Text w={"50%"} fontFamily={"Poppins,sans-serif"} textAlign={"center"} fontSize={"20px"} fontStyle={"normal"} fontWeight={"400"} mb={'2rem'} lineHeight={"normal"} color={"#8a8a8a"}>Every piece of AI technology shipped from LENS is thoroughly benchmarked via rigorous evaluations. With a global network of experts and academicians, we guarantee the most accurate and robust solutions in the market.</Text>
+        <Box display={"flex"}
+          alignItems={"center"}
+          justifyContent={'space-between'}
+          gap={'40px'}>
+          {/* 1st */}
+          <Box>
+
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>State-of-the-art solutions</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>Fast & Efficient</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>No extra computation fee</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>No licensing fee</Text>
+            </Box>
+          </Box>
+          {/* 2nd */}
+          <Box>
+
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>Lifetime support & upgrades</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>Plug-and-Play</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>24x7 Progress Monitoring</Text>
+            </Box>
+            <Box
+
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={400}
+
+              gap={'8px'}
+              fontSize={"18px"}
+              padding={2}
+              letterSpacing={'.622px'}>
+              <CheckIcon boxSize={3.5} />
+              <Text>Incremental Updates</Text>
+            </Box>
+          </Box>
+        </Box>
+        {/*  borderColor={'red'} borderWidth={'2px'} */}
+
+
+        <Box display={'flex'} justifyContent={'center'} gap={4} flexWrap={'wrap'} margin={'5rem 0'}>
+          {cardsData.map((card) => (
+            <Box key={card.id}
+              css={{
+                '&:hover': {
+                  '& img': {
+                    animationName: 'none',
+                  },
+                },
+              }}>
+              <Box
+                w={`${card.blurRadius.split('px')[0]}px`}
+                h={`${card.blurRadius.split('px')[0] / 2}px`}
+                borderRadius={'100px'}
+                filter={`blur(${card.blurRadius})`}
+                background={card.bgColor}
+                mt={card.offsetY}
+                ml={card.offsetX}
+                position={'absolute'}
+              />
+              <Box
+                w={'335px'}
+                h={'435px'}
+                bg={'#fff'}
+                padding={'1.6rem'}
+                borderRadius={'24px'}
+                display={'flex'}
+                flexDirection={'column'}
+                alignItems={'flex-start'}
+                justifyContent={'space-between'}
+                position={'relative'}
+                css={{
+                  '&:hover': {
+                    background: card.hoverBgGradient,
+                    outline: card.hoverOutline,
+                  },
+                }}
+              >
+                <Text display={'flex'} fontSize={'1.7rem'} fontWeight={'600'} padding={'0 1.5rem 1.5rem'}>
+                  {card.title}
+                </Text>
+                <Image
+                  src={card.imageUrl}
+                  css={{
+                    animationName: card.imageAnimation,
+                    animationDuration: '5s',
+                    animationTimingFunction: 'ease-in-out',
+                    animationIterationCount: 'infinite',
+                    animationDelay: '1s',
+
+                  }}
+                  alignSelf={'center'}
+                />
+                <Text color={'#828282'}>
+                  {card.description}
+                </Text>
+              </Box>
+            </Box>
           ))}
         </Box>
       </Box>
